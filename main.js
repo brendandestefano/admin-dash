64a1522287a0221ad80d936e2302a839e7b4503e
Brendan DeStefano <brendandestefano@Brendans-MacBook-Pro.local>
2016-06-15 10:16:09 -0400

    v3 initial commit

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-var checkOverlaySize = function($overlay, $image, border){
+var checkOverlaySize = function($overlay, $image, border, direction){
 	var border = border || false;
 	if(!$image.length)
 		return;
 
-	$overlay.css({
-		height: $image.height() + parseFloat(border.replace('px', ''))+'px',
-		bottom: '-'+($image.height() + parseFloat(border.replace('px', ''))+'px')
-	});
+	$overlay.css('height', $image.height() + (border ? parseFloat(border.replace('px', '')) : 0)+'px');
+
+	if(direction == 'bottom')
+		$overlay.css('bottom', '-'+($image.height() + (border ? parseFloat(border.replace('px', '')) : 0)+'px'));
+	else
+		$overlay.css('top', '-'+($image.height() +'px'));
 };
 
 jQuery(document).ready(function($){
-	var overlay = $('.vertical-navbar__overlay-wrapper');
-	var image = $('.vertical-navbar__profile-img');
+	var overlayBottom = $('.vertical-navbar__overlay-wrapper--profile');
+	var avatar = $('.vertical-navbar__profile-img');
 
-	checkOverlaySize(overlay, image, image.css('border-bottom-width'));
+	checkOverlaySize(overlayBottom, avatar, avatar.css('border-bottom-width'), 'bottom');
+	checkOverlaySize($('.vertical-navbar__overlay-wrapper--school-logo'), $('.school-logo'), false, 'top');
+	checkOverlaySize($('.vertical-navbar__school-logo-wrapper'), $('.school-logo'), false, 'top');
+
 
 	$(window).on('resize', function(){
-		checkOverlaySize(overlay, image, image.css('border-bottom-width'));
+		checkOverlaySize(overlayBottom, avatar, avatar.css('border-bottom-width'), 'bottom');
+		checkOverlaySize($('.vertical-navbar__overlay--school-logo'), $('.school-logo'), false, 'top');
+		checkOverlaySize($('.vertical-navbar__school-logo-wrapper'), $('.school-logo'), false, 'top');
 	});
 });
